1f4a866b105d5eca1edb9865296821bcc8142078
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-08-18 13:57:23 +0500

    learning

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,15 @@ public class Main {
         short myShort = 847;
         long myLong = 9797;
         double myDouble = 7.3243;
+        double myFloat = 324.3f;
+        double myChar = 'y';
 
         System.out.println(myNumber);
         System.out.println(myShort);
         System.out.println(myLong);
         System.out.println(myDouble);
+        System.out.println(myFloat);
+        System.out.println(myChar);
+
     }
 }
