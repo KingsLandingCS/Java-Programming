91760987134ab72a2c0aa6f7493699a50387285d
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-08-18 13:55:27 +0500

    learning

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int myNumber = 88;
+        short myShort = 847;
+        long myLong = 9797;
+        double myDouble = 7.3243;
+
+        System.out.println(myNumber);
+        System.out.println(myShort);
+        System.out.println(myLong);
+        System.out.println(myDouble);
+    }
+}
