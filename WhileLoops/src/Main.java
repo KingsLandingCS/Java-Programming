c880cc88e33d4dc858c40611fc6427d4625d1687
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-08-19 05:30:34 +0500

    learning

--- WhileLoops/src/Main.java
+++ WhileLoops/src/Main.java
@@ -2,6 +2,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        int value = 0;
+        while (value <= 10) {
+            System.out.println("hello " + value);
+            value = value + 1;
+        }
     }
 }
