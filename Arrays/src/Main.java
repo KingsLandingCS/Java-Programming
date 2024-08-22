ce34ca749175e4d13f6a1b78cbfbf56acb8a8b43
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-08-22 14:05:52 +0500

    learning

--- Arrays/src/Main.java
+++ Arrays/src/Main.java
@@ -4,14 +4,9 @@ public class Main {
     public static void main(String[] args) {
 
 //        int value = 7;
-
-        int[] values;
-        values = new int[3];
-
-        System.out.println(values[0]);
-        values[0] = 10;
-        values[1] = 20;
-        values[2] = 30;
-        System.out.println(values[0]);
+        int[] numbers = {5, 6, 7};
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
     }
 }
